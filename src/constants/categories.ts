cf5b72fe11258bb3ab3ede0445960bad61d45ac0
sensen312 <sensen312school@gmail.com>
2025-04-30 20:41:51 -0400

    Change of plans

---
+++ src/constants/categories.ts
@@ -0,0 +1,8 @@
+export const ITEM_CATEGORIES = [
+    { label: 'Shiny Bits', value: 'shiny' },
+    { label: 'Rocks', value: 'rock' },
+    { label: 'Trinkets', value: 'trinket' },
+    { label: 'Toys', value: 'toy' },
+];
+
+export const WANTS_CATEGORIES = [...ITEM_CATEGORIES];
