ab5dd4d093713e577886feb56c2b00c9052c0928
sensen312 <sensen312school@gmail.com>
2025-06-06 23:07:39 -0400

    aaa

--- GobTrades.Backend/obj/Debug/net9.0/GobTrades.Backend.AssemblyInfo.cs
+++ GobTrades.Backend/obj/Debug/net9.0/GobTrades.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GobTrades.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753b480a69764fc5853e125a9bf0c7f1fa4722b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567766b11c4ac2b1a528ca25d0d3f0702c1e8e0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GobTrades.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GobTrades.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
